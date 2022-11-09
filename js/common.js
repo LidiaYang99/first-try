06cd27718b906616d6692d7323b6ee06852262ba
Lidia <Lidiayang104@gmail.com>
2022-11-09 11:10:06 +0100

    轻提示

--- js/common.js
+++ js/common.js
@@ -5,3 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+// 轻提示封装包
+const toastBox = document.querySelector('#myToast')
+const toast = new bootstrap.Toast(toastBox, {
+  animation: true,
+  autohide: true,
+  delay: 10000
+})
+
+function tip(msg) {
+  toastBox.querySelector('.toast-body').innerHTML = msg
+}
+
+toast.show()
