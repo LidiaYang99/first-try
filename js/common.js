178102f0af84d3ad9670f940703da5157151827f
Lidia <Lidiayang104@gmail.com>
2022-11-24 14:41:16 +0100

    js更新版

--- js/common.js
+++ js/common.js
@@ -6,16 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 200)
 })
 
-// 轻提示封装包
+
 const toastBox = document.querySelector('#myToast')
 const toast = new bootstrap.Toast(toastBox, {
   animation: true,
   autohide: true,
-  delay: 10000
+  delay: 3000
 })
 
-function tip(msg) {
+const tip = (msg) => {
   toastBox.querySelector('.toast-body').innerHTML = msg
+  toast.show()
 }
 
-toast.show()
+let username = document.querySelector('.navbar .font-weight-bold')
+let logout = document.querySelector('#logout')
+
+// 因为这里是公共的js，登录页也会用到这些，所以如果不对是否有username进行判断，会在登录页报错
+if (username) {
+  username.innerHTML = localStorage.getItem('user-name')
+}
+
+if (logout) {
+  logout.addEventListener('click', () => {
+    localStorage.removeItem('user-token')
+    localStorage.removeItem('user-name')
+    location.href = './login.html'
+  })
+}
